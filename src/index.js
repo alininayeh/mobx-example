92dd671443aa76271d410d175ca336b38b1a632c
Alin Inayeh <alin.inayeh@lottoland.com>
2019-07-12 10:31:41 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {observer} from 'mobx-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+@observer
+class App extends React.Component {
+  render() {
+    const store = this.props.store;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    return (
+      <div>
+        app
+      </div>
+    );
+  }
+}
+
